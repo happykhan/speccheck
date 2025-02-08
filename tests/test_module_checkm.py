1cb5e441999f68b4c7048c412aea3d822ea90d04
Nabil-Fareed Alikhan <nabil@happykhan.com>
2025-02-08 12:02:00 +0000

    Added ariba and sylph

--- tests/test_module_checkm.py
+++ tests/test_module_checkm.py
@@ -1,22 +1,22 @@
 import pytest
 import os
-from speccheck.modules.checkm import CheckM
+from speccheck.modules.checkm import Checkm
 
 
 def test_has_valid_filename():
-    checkm = CheckM("test_file.tsv")
+    checkm = Checkm("test_file.tsv")
     assert checkm.has_valid_filename
 
-    checkm = CheckM("test_file.csv")
+    checkm = Checkm("test_file.csv")
     assert not checkm.has_valid_filename
 
 def test_has_valid_fileformat():
     checkm_file = "tests/collect_test_data/checkm.short.tsv"
-    checkm = CheckM(checkm_file)
+    checkm = Checkm(checkm_file)
     assert checkm.has_valid_fileformat
 
 def test_checkmvalues():
     checkm_file = "tests/collect_test_data/checkm.short.tsv"
-    checkm = CheckM(checkm_file)
+    checkm = Checkm(checkm_file)
     values = checkm.fetch_values()
     assert values['Marker lineage'] == 'Mycoplasma genitalium (6)'
