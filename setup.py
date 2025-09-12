8e75b5ab890095c8c42ba6787498f03de6b51b52
Nabil-Fareed Alikhan <nabil@happykhan.com>
2025-09-12 13:25:01 +0100

    getting ready for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
-from speccheck import __version__, __name__
+from speccheck import __version__, __author__, __email__, __description__, __url__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name=__name__,
+    name="Speccheck",
     version=__version__,
+    author=__author__,
+    description=__description__,
+    author_email=__email__,
+    url=__url__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         'rich',
@@ -17,11 +26,28 @@ setup(
             "pytest",
             "coverage",
             "pylint",
+            "twine",
+            "setuptools",
         ]
     },
     entry_points={
         'console_scripts': [
-            # Add command line scripts here
+            'speccheck=speccheck:main',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bioinformatics",
+    ],
+    python_requires=">=3.10",
+    license="GPLv3",
+    keywords="genomics qc bioinformatics",
+    project_urls={
+        "Documentation": "https://github.com/happykhan/speccheck",
+        "Source": "https://github.com/happykhan/speccheck",
+        "Tracker": "https://github.com/happykhan/speccheck/issues",
+    },    
 )
